31e655ca721c474576e3c00ed31349c60a0af275
SoftDiamond <30062102+TheSoftDiamond@users.noreply.github.com>
2025-02-02 02:49:58 -0800

    New Events

---
+++ BrutalCompanyMinus/Minus/ModdedEvents/ManStalker.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BrutalCompanyMinus.Minus.Events
+{
+    internal class ManStalker : MEvent
+    {
+        public override string Name() => nameof(ManStalker);
+
+        public static ManStalker Instance;
+
+        public override void Initalize()
+        {
+            Instance = this;
+
+            Weight = 2;
+            Descriptions = new List<string>() { "It stalks", "Hope you are not caught", "Avoid at all costs..." };
+            ColorHex = "#FF0000";
+            Type = EventType.Bad;
+
+            monsterEvents = new List<MonsterEvent>()
+            {
+                new MonsterEvent(
+                    "menstalker_type",
+                    new Scale(100.0f, 0.0f, 100.0f, 100.0f),
+                    new Scale(0.0f, 0.0f, 0.0f, 0.0f),
+                    new Scale(1.0f, 0.0f, 1.0f, 1.0f),
+                    new Scale(1.0f, 0.016f, 1.0f, 3.0f),
+                    new Scale(0.0f, 0.0f, 0.0f, 0.0f),
+                    new Scale(0.0f, 0.0f, 0.0f, 0.0f)),
+            };
+        }
+
+        public override bool AddEventIfOnly() => Compatibility.ManStalkerPresent;
+
+        public override void Execute() => ExecuteAllMonsterEvents();
+    }
+}
