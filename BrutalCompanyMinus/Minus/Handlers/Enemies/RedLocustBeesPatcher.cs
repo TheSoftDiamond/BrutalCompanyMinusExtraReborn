74d42f322e9fcf4142adfc2d83dd4439458e6f89
TI <115467358+tixomirof@users.noreply.github.com>
2025-02-03 02:40:50 +0500

    fix: fixed hive value multiplied twice
    Manager.scrapValueMultiplier multiplied hive scrap value twice, because method was called from server as server, and then from server as host's client (not sure exactly what was happening, but that has to be something with ClientRpc stuff). Now fixed, used HarmonyTranspiler instead.

--- BrutalCompanyMinus/Minus/Handlers/Enemies/RedLocustBeesPatcher.cs
+++ BrutalCompanyMinus/Minus/Handlers/Enemies/RedLocustBeesPatcher.cs
@@ -1,17 +1,36 @@
 ﻿using HarmonyLib;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Reflection;
 using UnityEngine;
-using Unity.Netcode;
 
 namespace BrutalCompanyMinus.Minus.Handlers.Enemies
 {
     [HarmonyPatch(typeof(RedLocustBees))]
     internal static class RedLocustBeesPatcher
     {
-        [HarmonyPrefix]
+        [HarmonyTranspiler]
         [HarmonyPatch(nameof(RedLocustBees.SpawnHiveClientRpc))]
-        internal static void IncreaseHiveScrapValue(NetworkObjectReference hiveObject, ref int hiveScrapValue, Vector3 hivePosition)
+        internal static IEnumerable<CodeInstruction> IncreaseHiveScrapValue(IEnumerable<CodeInstruction> instructions)
         {
-            hiveScrapValue = Mathf.RoundToInt(hiveScrapValue * Manager.scrapValueMultiplier);
+            var scrapValueMultiplier = typeof(Manager).GetField(nameof(Manager.scrapValueMultiplier), BindingFlags.Static | BindingFlags.NonPublic);
+            var roundMethod = typeof(Mathf).GetMethod(nameof(Mathf.RoundToInt));
+
+            var found = false;
+            foreach (var instruction in instructions)
+            {
+                if (instruction.IsLdarg(2) && !found)
+                {
+                    found = true;
+                    yield return new CodeInstruction(OpCodes.Ldarg_2);                          // push 'hiveScrapValue' to stack
+                    yield return new CodeInstruction(OpCodes.Conv_R4);                          // converting 'hiveScrapValue' to float32
+                    yield return new CodeInstruction(OpCodes.Ldsfld, scrapValueMultiplier);     // push bcmer multiplier to stack
+                    yield return new CodeInstruction(OpCodes.Mul);                              // multiply bcmer multiplier and 'hiveScrapValue'
+                    yield return new CodeInstruction(OpCodes.Call, roundMethod);                // round result to Int32, since 'hiveScrapValue' is an integer
+                    yield return new CodeInstruction(OpCodes.Starg, 2);                         // save result to 'hiveScrapValue'
+                }
+                yield return instruction;
+            }
         }
     }
 }
