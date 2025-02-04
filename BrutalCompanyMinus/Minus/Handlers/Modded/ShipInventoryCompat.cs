ee7ab0b303b24bcd811ce44a9e90cdc3c6eacb7f
SoftDiamond <30062102+TheSoftDiamond@users.noreply.github.com>
2025-02-03 22:19:12 -0800

    Ship Inventory Fix

---
+++ BrutalCompanyMinus/Minus/Handlers/Modded/ShipInventoryCompat.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BrutalCompanyMinus.Minus.Handlers.Modded
+{
+    internal class ShipInventoryCompat
+    {
+        public static int GetShipInventoryScrapValue()
+        {
+            try
+            {
+                int siCount = 0;
+
+                //Getting inventory from ship inventory
+                foreach (ShipInventory.Objects.ItemData data in ShipInventory.Helpers.ItemManager.GetItems())
+                {
+                    Item item = data.GetItem();
+                    if (item.isScrap)
+                    {
+                        siCount += data.SCRAP_VALUE;
+                    }
+                }
+                return siCount;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("ShipInventory types not found.");
+                int siCount = 0;
+                return siCount;
+            }
+        }
+    }
+}
+
