4a589ef423daa7be62cdc99f6e8b463fcf662501
SoftDiamond <30062102+TheSoftDiamond@users.noreply.github.com>
2025-02-05 03:06:16 -0800

    Create CustomEventHandling.cs

---
+++ BrutalCompanyMinus/Minus/Handlers/CustomEvents/CustomEventHandling.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BrutalCompanyMinus.Minus.Handlers.CustomEvents
+{
+    internal class CustomEventHandling
+    {
+    }
+}
