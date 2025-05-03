76eae8a71ffcc1b2ccb07007d62670bfea689f08
SoftDiamond <30062102+TheSoftDiamond@users.noreply.github.com>
2025-05-03 15:58:19 -0700

    fixes

--- BrutalCompanyMinus/Minus/Handlers/Flashlights.cs
+++ BrutalCompanyMinus/Minus/Handlers/Flashlights.cs
@@ -14,25 +14,41 @@ using System.IO;
 
 namespace BrutalCompanyMinus.Minus.Handlers
 {
-    [HarmonyPatch(typeof(FlashlightItem))]
-    internal class FlashlightItemPatches
+    public class FlashlightItemChargerPatches : NetworkBehaviour
     {
 
-        [HarmonyPostfix]
-        [HarmonyPatch("Update")]
-        private static void PreventItemActivation(FlashlightItem __instance)
+        // I literally tried several methods. This one isn't my favorite but... here we are.
+        // I really hate this spaghetti code. May I find a better solution one day.
+        public static FlashlightItemChargerPatches instance;
+        public void Awake()
         {
-            // Overwrite the door power and change the display text.
-            if (FlashLightNet.Value == 0)
+            if (instance != null) DestroyFlashlightFailure();
+            instance = this;
+            Net.Instance.SetFlashlightsServerRpc(true);
+        }
+
+        public static void DestroyFlashlightFailure() // Delete   
+        {
+            Events.FlashLightsFailure.Active = false;
+            GameObject flashlightObject = GameObject.Find("FlashlightsFailureObject");
+            if (flashlightObject != null)
             {
-                // Make sure the power is constantly maxed out.
-                __instance.flashlightBulb.intensity = 0;
-                __instance.insertedBattery.empty = true;
-                __instance.flashlightInterferenceLevel = 10;
-            
-
+                GameObject.Destroy(flashlightObject);
             }
+        }
 
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(StartOfRound), nameof(StartOfRound.ShipLeave))]
+        public static void OnShipLeave()
+        {
+            DestroyFlashlightFailure();
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(StartOfRound), nameof(StartOfRound.EndGameServerRpc))]
+        public static void DestroyForGodsSake()
+        {
+            DestroyFlashlightFailure();
         }
     }
 }
