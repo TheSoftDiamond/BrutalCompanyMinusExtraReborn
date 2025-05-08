4eb84b2dc836cce7ac84ff325ac4906b7e536a08
SoftDiamond <30062102+TheSoftDiamond@users.noreply.github.com>
2025-05-08 01:41:07 -0700

    0.24.8

---
+++ BrutalCompanyMinus/Minus/Handlers/Modded/PhonesOutPatches.cs
@@ -0,0 +1,44 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using Unity.Netcode;
+
+
+namespace BrutalCompanyMinus.Minus.Handlers.Modded
+{
+    public class PhonesOutPatches : NetworkBehaviour
+    {
+
+        public static PhonesOutPatches instance;
+
+        public void Awake()
+        {
+            if (instance != null) DestroyPhonesOut();
+            instance = this;
+            Net.Instance.SetPhonesOutServerRpc(true);
+        }
+
+        public static void DestroyPhonesOut() // Delete   
+        {
+            Events.PhonesOut.Active = false;
+            GameObject PhonesOutObject = GameObject.Find("PhonesOutFailureObject");
+            if (PhonesOutObject != null)
+            {
+                GameObject.Destroy(PhonesOutObject);
+            }
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(StartOfRound), nameof(StartOfRound.ShipLeave))]
+        public static void OnShipLeave()
+        {
+            DestroyPhonesOut();
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(StartOfRound), nameof(StartOfRound.EndGameServerRpc))]
+        public static void DestroyPhonesOutForGodsSake()
+        {
+            DestroyPhonesOut();
+        }
+    }
+}
---
+++ BrutalCompanyMinus/Minus/ModdedEvents/PhonesOut.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.Netcode;
+using UnityEngine;
+using HarmonyLib;
+using LethalNetworkAPI;
+using GameNetcodeStuff;
+using BrutalCompanyMinus.Minus.Handlers.Modded;
+using BrutalCompanyMinus;
+using Steamworks.Ugc;
+using BrutalCompanyMinus.Minus.Handlers;
+using System.ComponentModel.Design;
+using BrutalCompanyMinus.Minus.MonoBehaviours;
+using Scoops.misc;
+
+namespace BrutalCompanyMinus.Minus.Events
+{
+    [HarmonyPatch]
+    internal class PhonesOut : MEvent
+    {
+        public override string Name() => nameof(PhonesOut);
+
+        public static PhonesOut Instance;
+
+        public static bool Active = false;
+
+        public override void Initalize()
+        {
+            Instance = this;
+
+            Weight = 2;
+            Descriptions = new List<string>() { "Please leave a message after the beep..", "Bad reception", "Phone lines are down", "I think the phones are broken" };
+            ColorHex = "#FF0000";
+            Type = EventType.Bad;
+
+        }
+
+        public override bool AddEventIfOnly() => Compatibility.LethalPhonesPresent;
+
+        public override void Execute()
+        {
+
+            // Declare the Active state to true globally
+            Net.Instance.SetFlashlightsServerRpc(true);
+
+            // Bind the PhonesOut to an GameObject
+            GameObject PhonesOutObject = new GameObject("PhonesOutFailureObject");
+
+            // Add the PhonesOutPatches component to the GameObject
+            PhonesOutObject.AddComponent<PhonesOutPatches>();
+        }
+        public override void OnShipLeave()
+        {
+
+            // Reset the Active state
+            Active = false;
+        }
+        public override void OnGameStart()
+        {
+            // Reset the Active state
+            Active = false;
+        }
+
+        public override void OnLocalDisconnect()
+        {
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(PlayerPhone), nameof(PlayerPhone.ToggleActive))]
+        public static bool InterruptPhoneUsage(bool active)
+        {
+            // Interrupt the phone pickup
+            if (PhonesOut.Active)
+            {
+                HUDManager.Instance.globalNotificationText.text =
+                "BAD PHONE RECEPTION!!!!";
+
+                HUDManager.Instance.globalNotificationAnimator.SetTrigger("TriggerNotif");
+                HUDManager.Instance.UIAudio.PlayOneShot(
+                    HUDManager.Instance.radiationWarningAudio,
+                    1f
+                );
+                return false;
+
+            }
+
+            return true;
+        }
+
+    }
+}
