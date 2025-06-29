bf8ac1150eb7ae6ddf37dd092e37c3a54c14afee
SoftDiamond <30062102+TheSoftDiamond@users.noreply.github.com>
2025-06-28 21:34:41 -0700

    Coming Soon

---
+++ BrutalCompanyMinus/Minus/ModdedEvents/KidnapperFox.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BrutalCompanyMinus.Minus.Events
+{
+    internal class KidnapperFox : MEvent
+    {
+        public override string Name() => nameof(KidnapperFox);
+
+        public static KidnapperFox Instance;
+
+        public static bool Active = false;
+        public override void Initalize()
+        {
+            Instance = this;
+
+            Weight = 2;
+            Descriptions = new List<string>() { "YARHAR", "Five nights...?", "It will be a nightmare time" };
+            ColorHex = "#FF0000";
+            Type = EventType.Bad;
+        }
+
+        public override bool AddEventIfOnly() => Compatibility.KidnapperFoxPresent;
+
+        public override void Execute()
+        {
+
+        }
+    }
+}
