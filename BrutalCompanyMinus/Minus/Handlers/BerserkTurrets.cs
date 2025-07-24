fa0f570ba8faa3ccd1c56c1b93e2b1b2950ecde9
SoftDiamond <30062102+TheSoftDiamond@users.noreply.github.com>
2025-07-23 23:16:27 -0700

    Create BerserkTurrets.cs

---
+++ BrutalCompanyMinus/Minus/Handlers/BerserkTurrets.cs
@@ -0,0 +1,25 @@
+﻿using BrutalCompanyMinus.Minus.MonoBehaviours;
+using HarmonyLib;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using BrutalCompanyMinus.Minus.Events;
+
+namespace BrutalCompanyMinus.Minus.Handlers
+{
+    [HarmonyPatch(typeof(Turret))]
+    internal class BerserkTurrets
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch("Update")]
+        private static void TurretsHellMode(Turret __instance)
+        {
+            if (Events.BerserkTurrets.Active)
+            {
+                __instance.turretMode = TurretMode.Berserk;
+            }
+        }
+    }
+}
