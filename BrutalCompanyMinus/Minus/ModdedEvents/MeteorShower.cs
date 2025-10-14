f7345622bccedec1bb2877da111b07655cb32260
SoftDiamond <30062102+TheSoftDiamond@users.noreply.github.com>
2025-10-14 01:43:33 -0700

    events

---
+++ BrutalCompanyMinus/Minus/ModdedEvents/MeteorShower.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BrutalCompanyMinus.Minus.Events
+{
+    internal class MeteorShower : MEvent
+    {
+        public override string Name() => nameof(MeteorShower);
+
+        public static MeteorShower Instance;
+
+        public override void Initalize()
+        {
+            Instance = this;
+
+            Weight = 3;
+            Descriptions = new List<string>() { "A different kind of shower...", "More meteors?!", "What the.. Meteors..?" };
+            ColorHex = "#FF0000";
+            Type = EventType.Bad;
+
+            EventsToRemove = new List<string>() { nameof(SolarFlare), nameof(Windy), nameof(Gloomy), nameof(Raining), nameof(AllWeather) };
+        }
+
+        public override bool AddEventIfOnly() => Compatibility.CodeRebirthPresent;
+
+        public override void Execute()
+        {
+            if (Compatibility.CodeRebirthPresent)
+            {
+                Handlers.Modded.CustomWeather.SetCustomWeather("Meteor Shower");
+            }
+        }
+    }
+}
