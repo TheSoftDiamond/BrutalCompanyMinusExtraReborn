c637e6a8583d283e6c6f9492ef7ceee5655247fb
SoftDiamond <30062102+TheSoftDiamond@users.noreply.github.com>
2025-10-14 01:43:33 -0700

    events

---
+++ BrutalCompanyMinus/Minus/ModdedEvents/SolarFlare.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BrutalCompanyMinus.Minus.Events
+{
+    internal class SolarFlare : MEvent
+    {
+        public override string Name() => nameof(SolarFlare);
+
+        public static SolarFlare Instance;
+
+        public override void Initalize()
+        {
+            Instance = this;
+
+            Weight = 3;
+            Descriptions = new List<string>() { "Solar Flare Incoming!", "A massive solar flare is approaching!" };
+            ColorHex = "#FF0000";
+            Type = EventType.Bad;
+
+            EventsToRemove = new List<string>() { nameof(Heatwave), nameof(MeteorShower), nameof(Windy), nameof(Gloomy), nameof(Raining), nameof(AllWeather) };
+        }
+
+        public override bool AddEventIfOnly() => Compatibility.LethalElementsPresent;
+
+        public override void Execute()
+        {
+            if (Compatibility.LethalElementsPresent)
+            {
+                Handlers.Modded.CustomWeather.SetCustomWeather("Solar Flare");
+            }
+        }
+    }
+}
