3e49f73f36a0470ecfd9bb62b0b7973ec8b4347e
SoftDiamond <30062102+TheSoftDiamond@users.noreply.github.com>
2025-10-14 01:43:55 -0700

    handler

---
+++ BrutalCompanyMinus/Minus/Handlers/Modded/CustomWeather.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WeatherRegistry;
+using System.Linq;
+using static UnityEngine.Rendering.HighDefinition.ScalableSettingLevelParameter;
+
+namespace BrutalCompanyMinus.Minus.Handlers.Modded
+{
+    public class CustomWeather
+    {
+        public static void SetCustomWeather(string WeatherName)
+        {
+            if (!Compatibility.IsModPresent("mrov.WeatherRegistry"))
+            {
+                return;
+            }
+
+            // Find the weather by name
+            try
+            {
+                WeatherRegistry.Weather weather = WeatherManager.Weathers.First(w => w.Name.Equals(WeatherName, StringComparison.OrdinalIgnoreCase));
+                if (weather == null)
+                {
+                    Log.LogError($"Weather '{WeatherName}' not found.");
+                    return;
+                }
+                //WeatherController.ChangeCurrentWeather(weather);
+
+
+                WeatherManager.CurrentWeathers.SetWeather(StartOfRound.Instance.currentLevel, weather);
+            }
+            catch (Exception ex)
+            {
+                Log.LogError($"Error setting '{WeatherName}' weather: {ex.Message}");
+            }
+        }
+
+        public static void AddCustomWeather(string WeatherName)
+        {
+            if (!Compatibility.IsModPresent("mrov.WeatherRegistry"))
+            {
+                return;
+            }
+
+            try
+            {
+                WeatherRegistry.Weather weather = WeatherManager.Weathers.First(w => w.Name.Equals(WeatherName, StringComparison.OrdinalIgnoreCase));
+                if (weather == null)
+                {
+                    Log.LogError($"Weather '{WeatherName}' not found.");
+                    return;
+                }
+                WeatherController.AddWeatherEffect(weather);
+            }
+            catch (Exception ex)
+            {
+                Log.LogError($"Error adding '{WeatherName}' weather: {ex.Message}");
+            }
+        }
+    }
+}
