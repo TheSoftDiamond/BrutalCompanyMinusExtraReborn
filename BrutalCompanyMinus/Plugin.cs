f39a338e06c365a21f519af306567e66ccbceb06
SoftDiamond <30062102+TheSoftDiamond@users.noreply.github.com>
2025-10-25 22:49:34 -0700

    Build for 1.26.7

--- BrutalCompanyMinus/Plugin.cs
+++ BrutalCompanyMinus/Plugin.cs
@@ -26,7 +26,7 @@ namespace BrutalCompanyMinus
     {
         private const string GUID = "SoftDiamond.BrutalCompanyMinusExtraReborn";
         private const string NAME = "BrutalCompanyMinusExtraReborn";
-        private const string VERSION = "1.26.6";
+        private const string VERSION = "1.26.7";
 
         internal static Plugin Instance { get; private set; }
 
