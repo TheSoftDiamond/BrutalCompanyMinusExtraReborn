4b3a6c37c1057284caf41506e80509911d6987d9
SoftDiamond <30062102+TheSoftDiamond@users.noreply.github.com>
2025-11-04 22:13:42 -0800

    Cleanup

--- BrutalCompanyMinus/Minus/Handlers/Terminal.cs
+++ BrutalCompanyMinus/Minus/Handlers/Terminal.cs
@@ -17,32 +17,4 @@ namespace BrutalCompanyMinus.Minus.Handlers
             return true;
         }
     }  
-
-   /* [HarmonyPatch(typeof(StartOfRound))]
-    internal class TerminalPatchesNew
-    {
-
-
-        [HarmonyPrefix]
-        [HarmonyPatch("StartGame")]
-        private static void KillTerminal(Terminal __instance)
-        {
-            if (TerminalNet.Value == 0)
-            {
-                //  node.displayText =
-                //      "TERMINAL ERROR: ORDER BROADCAST FAILED\n\nFAILED TO BROADCAST ORDER COMMAND TO THE COMPANY STORE SERVICE DUE TO SHIP COMMS MALFUNCTION\n\n";
-
-                // node.terminalEvent = "ERROR";
-                BCMECodeSecurityCheck.Modules.EventRelatedStuff.KillTerminalEvent();
-                
-              //  __instance.PlayTerminalAudioServerRpc(3);
-
-             //   __instance.LoadNewNode(node);
-             //   __instance.QuitTerminal(true);
-             //   __instance.
-             /*   __instance.useCreditsCooldown = true;
-                __instance.numberOfItemsInDropship = 0;
-                __instance.buyableItemsList.SetValue(null, 0);
-                __instance.moonsCatalogueList.SetValue(null, 0);*/
-
 }
