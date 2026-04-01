814bde192cced184adef9fa0bccf30494451ab06
SoftDiamond <30062102+TheSoftDiamond@users.noreply.github.com>
2026-04-01 15:44:21 -0700

    Added Stingray Event

---
+++ BrutalCompanyMinus/Minus/Events/Stingray.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace BrutalCompanyMinus.Minus.Events
+{
+    internal class Stingray : MEvent
+    {
+        public override string Name() => nameof(Stingray);
+
+        public static Stingray Instance;
+
+        public override void Initalize()
+        {
+            Instance = this;
+
+            Weight = 3;
+            Descriptions = new List<string>() { "Goo!", "The goo is coming for you!" };
+            ColorHex = "#FF0000";
+            Type = EventType.Bad;
+
+            monsterEvents = new List<MonsterEvent>() { new MonsterEvent(
+                Assets.EnemyName.Stingray,
+                new Scale(7.0f, 1.0f, 7.0f, 12.0f),
+                new Scale(0.0f, 0.0f, 0.0f, 0.0f),
+                new Scale(1.0f, 0.44f, 1.0f, 2.0f),
+                new Scale(4.0f, 0.20f, 4.0f, 9.0f),
+                new Scale(0.0f, 0.0f, 0.0f, 0.0f),
+                new Scale(0.0f, 0.0f, 0.0f, 0.0f))
+            };
+        }
+
+        public override void Execute() => ExecuteAllMonsterEvents();
+    }
+}
