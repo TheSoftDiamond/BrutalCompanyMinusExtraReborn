da78bc670d3aec43d6327003517987a9fc6ee0c0
SoftDiamond <30062102+TheSoftDiamond@users.noreply.github.com>
2026-04-01 15:44:37 -0700

    CatsAndDogs

---
+++ BrutalCompanyMinus/Minus/Events/CatsAndDogs.cs
@@ -0,0 +1,43 @@
+﻿using BrutalCompanyMinus;
+using BrutalCompanyMinus.Minus.MonoBehaviours;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.Rendering.HighDefinition;
+using static BrutalCompanyMinus.Minus.MonoBehaviours.EnemySpawnCycle;
+
+namespace BrutalCompanyMinus.Minus.Events
+{
+    internal class CatsAndDogs : MEvent
+    {
+        public override string Name() => nameof(CatsAndDogs);
+
+        public static CatsAndDogs Instance;
+
+
+        public override void Initalize()
+        {
+            Instance = this;
+
+            Weight = 1;
+            Descriptions = new List<string>() { "Who let the Dogs (And Cats) Out?", "It's raining Cats And Dogs", "It's raining Dogs And Cats" };
+            ColorHex = "#000000";
+            Type = EventType.VeryBad;
+
+            EventsToSpawnWith = new List<string>() { nameof(Dogs), nameof(Puma) };
+        }
+
+        public override void Execute()
+        {
+          
+        }
+
+        public override void OnShipLeave() { }
+
+        public override void OnGameStart() { }
+    }
+}
