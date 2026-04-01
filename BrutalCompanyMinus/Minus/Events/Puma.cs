ddc9386f05e63acc4bdab0ab45f773a300421287
SoftDiamond <30062102+TheSoftDiamond@users.noreply.github.com>
2026-04-01 15:44:29 -0700

    Added Puma

---
+++ BrutalCompanyMinus/Minus/Events/Puma.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace BrutalCompanyMinus.Minus.Events
+{
+    internal class Puma : MEvent
+    {
+        public override string Name() => nameof(Puma);
+
+        public static Puma Instance;
+
+        public override void Initalize()
+        {
+            Instance = this;
+
+            Weight = 3;
+            Descriptions = new List<string>() { "Kitties", "They exist for hunting you..", "Puma.. and we don't mean the brand." };
+            ColorHex = "#FF0000";
+            Type = EventType.Bad;
+
+            monsterEvents = new List<MonsterEvent>() { new MonsterEvent(
+                Assets.EnemyName.Puma,
+                new Scale(0.0f, 0.0f, 0.0f, 0.0f),
+                new Scale(5.0f, 0.2f, 5.0f, 25.0f),
+                new Scale(0.0f, 0.0f, 0.0f, 0.0f),
+                new Scale(0.0f, 0.0f, 0.0f, 0.0f),
+                new Scale(1.0f, 0.0f, 1.0f, 1.0f),
+                new Scale(2.0f, 0.60f, 2.0f, 6.0f))
+            };
+        }
+
+        public override void Execute() => ExecuteAllMonsterEvents();
+    }
+}
