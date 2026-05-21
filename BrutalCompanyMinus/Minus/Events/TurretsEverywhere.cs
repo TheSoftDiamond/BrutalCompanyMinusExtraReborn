e50094efb7e5cb772c31605f4602b4afaf615fa1
SoftDiamond <30062102+TheSoftDiamond@users.noreply.github.com>
2026-05-20 23:45:26 -0700

    fix

--- BrutalCompanyMinus/Minus/Events/TurretsEverywhere.cs
+++ BrutalCompanyMinus/Minus/Events/TurretsEverywhere.cs
@@ -27,29 +27,8 @@ namespace BrutalCompanyMinus.Minus.Events
 
             EventsToSpawnWith = new List<string>() { nameof(OutsideTurrets), nameof(Trees), nameof(Mantitoil) };
 
-           // scrapTransmutationEvent = new ScrapTransmutationEvent(
-           //     new Scale(0.5f, 0.008f, 0.5f, 0.9f),
-          //      new SpawnableItemWithRarity() { spawnableItem = Assets.GetItem(Assets.ItemName.AirHorn), rarity = 50 },
-           //     new SpawnableItemWithRarity() { spawnableItem = Assets.GetItem(Assets.ItemName.MappingDevice), rarity = 10 }
-          //  );
-
-          //  ScaleList.Add(ScaleType.ScrapAmount, new Scale(1.0f, 0.005f, 1.0f, 1.5f));
         }
 
         public override bool AddEventIfOnly() => Compatibility.toilheadPresent;
-        //  {
-        //      if (!Manager.transmuteScrap)
-        //      {
-        //          Manager.transmuteScrap = true;
-        //          return true;
-        //      }
-        //      return false;
-        //  }
-
-        //  public override void Execute()
-        //  {
-        //      Manager.scrapAmountMultiplier *= Getf(ScaleType.ScrapAmount);
-        //      scrapTransmutationEvent.Execute();
-        //  }
     }
 }
