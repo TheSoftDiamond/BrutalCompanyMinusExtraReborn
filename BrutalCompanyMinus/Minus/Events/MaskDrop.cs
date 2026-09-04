0cd21bb7406c3dc18ea2c6736756dd5ef43924cf
SoftDiamond <30062102+TheSoftDiamond@users.noreply.github.com>
2026-09-03 21:33:08 -0700

    maskdrop

---
+++ BrutalCompanyMinus/Minus/Events/MaskDrop.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using BrutalCompanyMinus.Minus.Handlers;
+using BrutalCompanyMinus.Minus.MonoBehaviours;
+using GameNetcodeStuff;
+using HarmonyLib;
+using Unity.Netcode;
+using UnityEngine;
+using static BrutalCompanyMinus.Net;
+
+namespace BrutalCompanyMinus.Minus.Events
+{
+    [HarmonyPatch]
+    internal class MaskDrop : MEvent
+    {
+        public override string Name() => nameof(MaskDrop);
+
+        public static MaskDrop Instance;
+
+        public override void Initalize()
+        {
+            Instance = this;
+
+            Weight = 1;
+            Descriptions = new List<string>() { "You are not alone", "WARNING: SHIPS ARE NEARBY", "They came from outer space.", "Out of the world" };
+            ColorHex = "#8B008B";
+            Type = EventType.Insane;
+            isSpecialEvent = true;
+            showTip = true;
+            TipTitle = new List<string>() { "WARNING", "ALERT", "DANGER" };
+            TipMessages = new List<string>() { "UNAUTHORIZED SHIPS DETECTED", "DETECTING RADIO WAVES", "WHO ARE THEY...?", "UNAUTHORIZED SHIPS IN ORBIT" };
+
+            ScaleList.Add(ScaleType.MinIntervalTime, new Scale(20.0f, -0.015f, 10.0f, 20.0f));
+            ScaleList.Add(ScaleType.MaXIntervalTime, new Scale(40.0f, -0.035f, 20.0f, 40.0f));
+            ScaleList.Add(ScaleType.Percentage, new Scale(70.0f, 3.55f, 70.0f, 100.0f));
+            ScaleList.Add(ScaleType.SpeedMin, new Scale(30.0f, 0.015f, 30.0f, 40.0f));
+            ScaleList.Add(ScaleType.SpeedMax, new Scale(50.0f, 0.035f, 50.0f, 70.0f));
+            ScaleList.Add(ScaleType.DistanceMax, new Scale(5.0f, 0.015f, 5.0f, 5.0f));
+            ScaleList.Add(ScaleType.MinSpawned, new Scale(0.0f, 0.124f, 0.0f, 1.0f));
+            ScaleList.Add(ScaleType.MaxSpawned, new Scale(2.0f, 0.57f, 2.0f, 4.0f));
+            ScaleList.Add(ScaleType.TimeWaitMinDespawn, new Scale(2.0f, 0.0f, 2.0f, 2.0f));
+            ScaleList.Add(ScaleType.TimeWaitMaxDespawn, new Scale(5.0f, 0.0f, 5.0f, 5.0f));
+            ScaleList.Add(ScaleType.timeStart, new Scale(0.0f, 0.0f, 0.0f, 0.0f));
+            ScaleList.Add(ScaleType.timeEnd, new Scale(1.0f, 0.0f, 1.0f, 1.0f));
+        }
+
+        public override void Execute()
+        {
+            Net.Instance.SetEventActiveServerRPC(Name(), true);
+
+            GameObject MaskDropObj = new GameObject("MaskDropObj");
+
+            MaskDropObj.AddComponent<MaskDropNet>();
+        }
+
+        public override void OnShipLeave()
+        {
+            // Reset the Active state
+            Active = false;
+        }
+        public override void OnGameStart()
+        {
+            // Reset the Active state
+            Active = false;
+        }
+
+        public override void OnLocalDisconnect()
+        {
+        }
+    }
+}
---
+++ BrutalCompanyMinus/Minus/Handlers/MaskDropNet.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Unity.Netcode;
+using UnityEngine;
+using HarmonyLib;
+using BepInEx.Configuration;
+using BrutalCompanyMinus.Minus.Events;
+using static BrutalCompanyMinus.Minus.MEvent;
+using System.Collections;
+using DunGen;
+using UnityEngine.UIElements;
+using UnityEngine.AI;
+using Dawn;
+
+namespace BrutalCompanyMinus.Minus.MonoBehaviours
+{
+    [HarmonyPatch]
+    internal class MaskDropNet : NetworkBehaviour
+    {
+        public static MaskDropNet instance;
+
+        public float nextDropTime;
+
+        public float currentTime;
+
+        public void Awake()
+        {
+            if (instance != null) DestroyInstance();
+            instance = this;
+            Net.Instance.SetEventActiveServerRPC(nameof(MaskDrop), true);
+
+            if (RoundManager.Instance.IsServer)
+            {
+                currentTime = 0f;
+
+                nextDropTime = UnityEngine.Random.Range(MaskDrop.Instance.Getf(ScaleType.MinIntervalTime), MaskDrop.Instance.Getf(ScaleType.MaXIntervalTime));
+                //Log.LogInfo($"[MaskDrop] Next drop time: {nextDropTime}");
+            }
+
+            if (RoundManager.Instance.outsideAINodes == null)
+            {
+                RoundManager.Instance.outsideAINodes = GameObject.FindGameObjectsWithTag("OutsideAINode");
+            }
+
+            //shipPrefab = new GameObject("ship");
+
+          
+
+            //Material blueMat = new Material(Shader.Find("HDRP/Lit"));
+            //blueMat.SetColor("_BaseColor", Color.blue);
+
+            //GameObject startSphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            //startSphere.name = "StartSphere";
+            //startSphere.transform.SetParent(shipPrefab.transform, false);
+            //startSphere.GetComponent<MeshRenderer>().material = blueMat;
+
+            //shipPrefab.SetActive(false);
+        }
+
+        public void Update()
+        {
+            if (RoundManager.Instance.IsServer)
+            {
+                if (!StartOfRound.Instance.shipHasLanded) return;
+
+                float theTimeOfDay = TimeOfDay.Instance.normalizedTimeOfDay;
+
+                if (theTimeOfDay <= MaskDrop.Instance.Getf(ScaleType.timeStart) || theTimeOfDay >= MaskDrop.Instance.Getf(ScaleType.timeEnd)) return;
+
+                currentTime += Time.deltaTime;
+                //Log.LogInfo($"[MaskDrop] Current time: {currentTime}, Next drop time: {nextDropTime}");
+                if (currentTime > nextDropTime)
+                {
+                    nextDropTime = UnityEngine.Random.Range(MaskDrop.Instance.Getf(ScaleType.MinIntervalTime), MaskDrop.Instance.Getf(ScaleType.MaXIntervalTime));
+                    currentTime = 0f;
+
+                    float chance = UnityEngine.Random.Range(0f, MaskDrop.Instance.Getf(ScaleType.Percentage));
+                    //Log.LogInfo($"[MaskDrop] Chance: {chance}, Percentage: {MaskDrop.Instance.Getf(ScaleType.Percentage)}");
+
+                    if (chance <= MaskDrop.Instance.Getf(ScaleType.Percentage))
+                    {
+                        int seed = UnityEngine.Random.Range(int.MinValue, int.MaxValue);
+
+                        int height = UnityEngine.Random.Range(170, 220);
+
+                        float speed = UnityEngine.Random.Range(MaskDrop.Instance.Getf(ScaleType.SpeedMin), MaskDrop.Instance.Getf(ScaleType.SpeedMax));
+                        //Log.LogInfo($"[MaskDrop] Spawning ship with seed: {seed}, height: {height}, speed: {speed}");
+
+                        float x = UnityEngine.Random.Range(0, 360);
+                        float y = UnityEngine.Random.Range(0, 360);
+                        float z = UnityEngine.Random.Range(0, 360);
+                        float timeToDespawn = UnityEngine.Random.Range(MaskDrop.Instance.Getf(ScaleType.TimeWaitMinDespawn), MaskDrop.Instance.Getf(ScaleType.TimeWaitMaxDespawn));
+
+                        Net.Instance.SpawnMaskShipServerRpc(seed, height, speed, x, y, z, timeToDespawn);
+                    }
+                }
+            }
+        }
+
+        public static void DestroyInstance() // This handles the deletion of Time Chaosness
+        {
+            Events.MaskDrop.Instance.Active = false;
+            GameObject netObject = GameObject.Find("MaskDropObj");
+            if (netObject != null)
+            {
+                GameObject.Destroy(netObject);
+            }
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(StartOfRound), nameof(StartOfRound.ShipLeave))]
+        public static void OnShipLeave()
+        {
+            DestroyInstance();
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(StartOfRound), nameof(StartOfRound.EndGameServerRpc))]
+        public static void DestroyForGodsSake()
+        {
+            DestroyInstance();
+        }
+    }
+}
